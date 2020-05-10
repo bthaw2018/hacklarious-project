0e478395a5141bb50bee6ab52c86d39bc41481e5
Benjamin Thaw <bthaw2018@fau.edu>
2020-05-10 01:54:37 -0400

    random pokemon and styling

--- website/public/scripts/chat.js
+++ website/public/scripts/chat.js
@@ -57,6 +57,15 @@ query.onSnapshot(snapshot => {
     })
 })
 randomMeme();
+randomPokemon();
+function randomPokemon() {
+    getRandomPokemon().then(res => {
+        var pokeContainer = document.querySelector("#randomPoke");
+        pokeContainer.querySelector("#poke_img").setAttribute("src", res.image);
+        pokeContainer.querySelector("#poke_name").textContent = res.name
+        pokeContainer.querySelector("#poke_id").textContent = `(${res.id})`
+    })
+}
 function randomMeme(intentionallyCauseAnError = false) {
     if (intentionallyCauseAnError == true) {
         var errorCodes = [100, 101, 102, 0, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 444, 451, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511, 599]
@@ -239,4 +248,41 @@ function getRandomMeme() {
 // https://http.cat/ By Versió Catalià, @rogeriopvl, and @girlie_mac
 function getHttpCatMeme(statusCode) {
     return "https://http.cat/" + statusCode + ".jpg";
+}
+
+function getRandomPokemon() {
+    return getRandomPokemonRequest().then(res => {
+        return {
+            id: res.id,
+            name: res.name,
+            image: res.sprites.front_default
+        }
+    })
+}
+function getRandomPokemonRequest() {
+    //1-807
+    //10001 - 10157
+    // 1/9 
+
+    var random1 = (Math.floor(Math.random() * Math.floor(806)));
+    var random2 = (Math.floor(Math.random() * Math.floor(155)));
+    var random3 = (Math.floor(Math.random() * Math.floor(9)));
+    var num;
+
+    if (random3 == 9) {
+        num = random2;
+    } else {
+        num = random1;
+    }
+    return $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/' + num,
+        success: data => {
+            return data;
+        },
+        error: (jqXHR, exception) => {
+            // console.log(jqXHR.status);
+            last_status = jqXHR.status;
+            // return getHttpCatMeme(jqXHR.status);
+        }
+    })
 }
