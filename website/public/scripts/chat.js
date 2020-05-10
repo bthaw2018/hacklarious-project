61d06c6308df4793285eb4fe7c54ab5ab5710a2a
Benjamin Thaw <bthaw2018@fau.edu>
2020-05-10 01:04:20 -0400

    added way to get a http cat meme, styling fixes, buttons for random memes

--- website/public/scripts/chat.js
+++ website/public/scripts/chat.js
@@ -57,7 +57,14 @@ query.onSnapshot(snapshot => {
     })
 })
 randomMeme();
-function randomMeme() {
+function randomMeme(intentionallyCauseAnError = false) {
+    if (intentionallyCauseAnError == true) {
+        var errorCodes = [100, 101, 102, 0, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 426, 428, 429, 431, 444, 451, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511, 599]
+        var random = (Math.floor(Math.random() * Math.floor(errorCodes.length)));
+        console.log(random);
+        document.querySelector("#randomMeme").setAttribute("src", getHttpCatMeme(errorCodes[random]));
+        return;
+    }
     getRandomMeme().then(response => {
         // console.log(response.url);
         document.querySelector("#randomMeme").setAttribute("src", response.url);
